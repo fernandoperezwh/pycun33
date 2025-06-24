eebda358f619ffe19cdc8670d91c957b75a6025c
FernandoPrzGmz <fernando.perez@wisphub.net>
2025-06-24 14:37:25 -0500

    a

--- src/views.py
+++ src/views.py
@@ -12,3 +12,8 @@ async def async_view(request):
 async def fake_async_view_with_blocking(request):
     time.sleep(5)
     return HttpResponse("Vista async mal implementada: bloqueó el event loop.")
+
+
+async def real_async_view(request):
+    await asyncio.sleep(5)  # no bloquea el event loop
+    return HttpResponse("Vista async completada.")
